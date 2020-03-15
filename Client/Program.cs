e1248fcc4138922c03438217bfe0f47085f5e292
Dmitrii Saburov <Saburov.98@mail.ru>
2020-03-15 14:57:55 +0300

    Пример взаимодействия клиента и сервера на udp

---
+++ Client/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static int localPort = 4004;
+        static Socket socket;
+        //Socket listenSocket;
+
+        public Client()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Console.WriteLine("Введите порт");
+            localPort = Int32.Parse(Console.ReadLine());
+        }
+        public void Start()
+        {
+            try
+            {
+                //listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listening);
+                listeningTask.Start();
+
+                //отправка сообщений на разные порты
+                while (true)
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                    socket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+        private void SendMessage(string mes, EndPoint remotePoint)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(mes);
+            socket.SendTo(data, remotePoint);
+
+            Console.WriteLine("отправил");
+        }
+
+        public void Listening()
+        {
+            try
+            {
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), localPort);
+                socket.Bind(localIP);
+
+                while (true)
+                {
+                    // получаем ответ
+                    byte[] data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine("ответ сервера: " + builder.ToString());
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+        private void Close()
+        {
+            if (socket != null)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                socket = null;
+            }
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        int port = 8005; // порт для приема входящих запросов
+        Socket listenSocket;
+
+        List<EndPoint> players = new List<EndPoint>();
+
+        public Server()
+        {
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+        public void Start()
+        {
+            try
+            {
+                //listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(Listening);
+                listeningTask.Start();
+
+                //отправка сообщений на разные порты
+                while (true)
+                {
+                    string message = Console.ReadLine();
+
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                    listenSocket.SendTo(data, remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+        private void SendMessage(string mes, EndPoint remotePoint)
+        {
+            players.Add(remotePoint);
+            byte[] data = Encoding.Unicode.GetBytes(mes);
+
+            foreach(var p in players)
+            {
+                listenSocket.SendTo(data, p);
+            }
+            //listenSocket.SendTo(data, remotePoint);
+
+            Console.WriteLine("отправил");
+        }
+
+        public void Listening()
+        {
+            try
+            {
+
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, port);
+                Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            
+            
+                listenSocket.Bind(ipPoint);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    
+
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256];// буфер для получаемых данных
+
+
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+                    do
+                    {
+                        bytes = listenSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listenSocket.Available > 0);
+
+
+
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    // выводим сообщение
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(),
+                                                    remoteFullIp.Port, builder.ToString());
+                    SendMessage($"{remoteFullIp.Address.ToString()} : {remoteFullIp.Port} - {builder.ToString().ToUpper()}", remoteFullIp);
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+        private void Close()
+        {
+            if (listenSocket != null)
+            {
+                listenSocket.Shutdown(SocketShutdown.Both);
+                listenSocket.Close();
+                listenSocket = null;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Start();
+        }
+    }
+}
